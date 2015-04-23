e01316ad2757a32bf42de21797e99026bd292546
rndom <rndom.hack@gmail.com>
2015-04-23 13:58:40 +0900

    add sendRegistGUI etc

--- sample/send.js
+++ sample/send.js
@@ -6,18 +6,10 @@ var ctrlCmdUtil = new CtrlCmdUtil({
     port: 4510
 });
 
-/*
-ctrlCmdUtil.sendReloadSetting().then((data) => {
-    console.log("success");
-}).catch((err) => {
-    console.log(err);
-});
-*/
-
-ctrlCmdUtil.sendEnumReserve2().then((data) => {
-    return ctrlCmdUtil.sendGetReserve2(data[data.length - 1].reserveID);
+ctrlCmdUtil.sendEnumReserve().then((data) => {
+    return ctrlCmdUtil.sendGetReserve(data[data.length - 1].reserveID);
 }).then((data) => {
     console.log(data);
 }).catch((err) => {
-    console.log(err);
+    console.log(err.stack);
 });
